6d2327ab746615fd40e89e928ca622bee26899a9
Felipe David <felipe.besorean@hotmail.com>
2022-07-15 13:22:34 -0300

    fix: lint

--- back-end/src/routes/routes.js
+++ back-end/src/routes/routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const userController = require('../controllers/userController');
 const { validateEmail, validatePassword } = require('../middlewares/userValidations');
-
 const routes = express.Router();
 
 routes.post(
