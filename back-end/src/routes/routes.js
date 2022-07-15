1fb162329e3bae5267addec00b9f5b91d31dcb21
Felipe David <felipe.besorean@hotmail.com>
2022-07-15 13:11:55 -0300

    feat: adiciona rota de login token jwt e validacoes

---
+++ back-end/src/routes/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const { validateEmail, validatePassword } = require('../middlewares/userValidations');
+
+const routes = express.Router();
+
+routes.post(
+  '/login',
+  validateEmail,
+  validatePassword,
+  userController.login,
+);
+
+module.exports =  routes;
