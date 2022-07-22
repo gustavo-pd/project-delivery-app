98d9ec5b3c77e641fa53f3827ea4cc5835c0e115
Pedro Lima <pedro.oficial1969@gmail.com>
2022-07-22 10:16:52 -0300

    feat(List-Order): adiciona estrutura de lista para página de vendas.

--- front-end/src/Pages/Order/index.jsx
+++ front-end/src/Pages/Order/index.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
-// import sales from '../../utils/mockSales';
+// import COMPONENTS from '../../Components';
+import sales from '../../utils/mockSales';
 
 const Order = () => (
-  <h1>Page Order</h1>
+  <div>
+    {/* <COMPONENTS.Header page="customer" /> */}
+    <article>
+      {sales.map((elements, index) => (
+        <div key={ index }>
+          <span>{elements.id}</span>
+          <h4>{elements.status}</h4>
+          <p>{elements.date}</p>
+          <p>{elements.price}</p>
+        </div>
+      ))}
+    </article>
+  </div>
 );
 
 export default Order;
--- front-end/src/utils/mockSales.js
+++ front-end/src/utils/mockSales.js
@@ -1,19 +1,19 @@
 const sales = [
   {
     id: 1,
-    date: new Date(),
+    date: '01/00/0000',
     price: 23.80,
     status: 'Pendente',
   },
   {
     id: 2,
-    date: new Date(),
+    date: '00/01/0000',
     price: 14.20,
     status: 'Preparando',
   },
   {
-    id: 1,
-    date: new Date(),
+    id: 3,
+    date: '00/00/0001',
     price: 28.46,
     status: 'Entregue',
   },
