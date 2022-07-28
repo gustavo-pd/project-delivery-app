8b59f1131bc828a728141ccdcff154371973d4a3
LuisCarlosCruz <luizcruz0809@gmail.com>
2022-07-27 23:10:47 -0300

    toma

--- front-end/src/Components/Table/index.jsx
+++ front-end/src/Components/Table/index.jsx
@@ -4,7 +4,7 @@ import './table.css';
 import { MainContext } from '../../store';
 import { getLocalStorage, setLocalStorage } from '../../utils/localStorage';
 
-export default function Table({ data, removeButton = true }) {
+export default function Table({ data, removeButton = true, infoSale }) {
   const [items, setItems] = useState([]);
   const {
     setTotalValue, setDisableButtonCart, setSubmitItems,
@@ -27,7 +27,6 @@ export default function Table({ data, removeButton = true }) {
       .reduce((prevValue, currValue) => (
         prevValue + (currValue.quantity * currValue.price)), 0);
     setTotalValue(formatPrice(total));
-
     if (total !== 0) setDisableButtonCart(false);
     if (total === 0) setDisableButtonCart(true);
   };
@@ -47,10 +46,49 @@ export default function Table({ data, removeButton = true }) {
   };
 
   const teste = 'customer_checkout__element-order-table-item-number-';
+  const dataTest = 'customer_order_details__';
 
   return (
     <table>
       <tbody>
+        {
+          !removeButton && (
+            <tr>
+              <th
+                data-testid={ `${dataTest}element-order-details-label-order-id` }
+              >
+                PEDIDO:
+                { ` 000${infoSale.sale.id}` }
+              </th>
+              <th
+                data-testid={ `${dataTest}element-order-details-label-seller-name` }
+              >
+                P. vend:
+                { infoSale.seller.name }
+              </th>
+              <th
+                data-testid={ `${dataTest}element-order-details-label-order-date` }
+              >
+                { new Date(infoSale.sale.saleDate).toLocaleDateString('pt-BR') }
+              </th>
+              <th
+                data-testid={ `${dataTest}element-order-details-label-delivery-status` }
+              >
+                { infoSale.sale.status }
+              </th>
+              <th>
+                <button
+                  disabled
+                  type="button"
+                  data-testid="customer_order_details__button-delivery-check"
+                >
+                  MARCAR COMO ENTREGUE
+                </button>
+              </th>
+            </tr>
+          )
+        }
+
         <tr>
           <th>Item</th>
           <th>Descrição</th>
@@ -64,31 +102,41 @@ export default function Table({ data, removeButton = true }) {
         {items.length > 0 && items.map((item, index) => (
           <tr key={ item.id }>
             <td
-              data-testid={ `${teste}-${index}` }
+              data-testid={ !removeButton
+                ? `${dataTest}element-order-table-item-number-${index}`
+                : `${teste}-${index}` }
             >
               {index + 1}
 
             </td>
             <td
-              data-testid={ `customer_checkout__element-order-table-name-${index}` }
+              data-testid={ !removeButton
+                ? `${dataTest}element-order-table-name-${index}`
+                : `customer_checkout__element-order-table-name-${index}` }
             >
               {item.name}
 
             </td>
             <td
-              data-testid={ `customer_checkout__element-order-table-quantity-${index}` }
+              data-testid={ !removeButton
+                ? `${dataTest}element-order-table-quantity-${index}`
+                : `customer_checkout__element-order-table-quantity-${index}` }
             >
               {!removeButton ? item.salesProducts.quantity : item.quantity}
 
             </td>
             <td
-              data-testid={ `customer_checkout__element-order-table-unit-price-${index}` }
+              data-testid={ !removeButton
+                ? `${dataTest}element-order-table-unit-price-${index}`
+                : `customer_checkout__element-order-table-unit-price-${index}` }
             >
               {formatPrice(item.price)}
 
             </td>
             <td
-              data-testid={ `customer_checkout__element-order-table-sub-total-${index}` }
+              data-testid={ !removeButton
+                ? `${dataTest}element-order-table-sub-total-${index}`
+                : `customer_checkout__element-order-table-sub-total-${index}` }
             >
               { !removeButton ? subtotal(item
                 .price, item.salesProducts.quantity) : subtotal(item
@@ -121,10 +169,22 @@ Table.propTypes = {
       price: PropTypes.string,
       quantity: PropTypes.number,
     }),
-  ).isRequired,
+  ),
+  infoSale: PropTypes.shape({
+    sale: PropTypes.shape({
+      id: PropTypes.number,
+      saleDate: PropTypes.string,
+      status: PropTypes.string,
+    }),
+    seller: PropTypes.shape({
+      name: PropTypes.string,
+    }),
+  }),
   removeButton: PropTypes.bool,
 };
 
 Table.defaultProps = {
   removeButton: true,
+  data: {},
+  infoSale: {},
 };
--- front-end/src/Pages/Details/index.jsx
+++ front-end/src/Pages/Details/index.jsx
@@ -4,28 +4,49 @@ import COMPONENTS from '../../Components';
 import { MainContext } from '../../store';
 
 const Details = () => {
+  const infoSaleInicial = {
+    sale: {
+      Products: [],
+      id: 1,
+      saleDate: '2022-07-28T01:18:26.000Z',
+      status: 'Pendente',
+    },
+    seller: {
+      name: '',
+    },
+  };
+
   const { totalValue } = useContext(MainContext);
   const [items, setItems] = useState([]);
+  const [infoSale, setInfoSale] = useState(infoSaleInicial);
 
   useEffect(() => {
     const url = window.location.pathname;
     const id = url.split('/')[3];
     const URL = 'http://localhost:3001';
+
     const request = async () => {
       const sales = await axios.get(`${URL}/sales/${id}`);
+      setInfoSale(sales.data);
       const { Products } = sales.data.sale;
       setItems(Products);
     };
     request();
-  }, [items]);
+  }, []);
 
   return (
     <div>
       <COMPONENTS.Header page="customer" />
       <h1>Detalhe do Pedido</h1>
       <section>
-        <COMPONENTS.Table data={ items } removeButton={ false } />
-        <h1>
+        <COMPONENTS.Table
+          data={ items }
+          infoSale={ infoSale }
+          removeButton={ false }
+        />
+        <h1
+          data-testid="customer_order_details__element-order-total-price"
+        >
           Total: R$
           {' '}
           { totalValue }
